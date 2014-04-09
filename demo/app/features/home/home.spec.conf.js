66d1818cc8d0ef7910927fa79a9af8ecae188f1d
Gregory Pratt <gregory.pratt@me.com>
2014-04-09 23:13:18 +0100

    Added integer tests and moved the grunt file

---
+++ demo/app/features/home/home.spec.conf.js
@@ -0,0 +1,21 @@
+exports.config = {
+    // The address of a running selenium server.
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+
+    // Capabilities to be passed to the web-driver instance.
+    capabilities: {
+        'browserName': 'chrome'
+    },
+
+    // Spec patterns are relative to the current working directly when
+    // protractor is called.
+    specs: ['*.spec.js'],
+
+    // Options to be passed to Jasmine-node.
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000
+    },
+
+    baseUrl: 'http://localhost:63342/' // Change this to whatever server you're running on locally
+};
