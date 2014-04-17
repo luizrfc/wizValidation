f039151a38566a55772561664f2b666f81128dba
Chris Renton <c.renton@ntlworld.com>
2014-04-17 15:52:16 +0100

    RequireOther refire remote validation

--- wizValidation/src/requireOther/requireOther.dir.js
+++ wizValidation/src/requireOther/requireOther.dir.js
@@ -1,31 +1,38 @@
 ﻿angular.module('wiz.validation.requireOther')
 
-	.directive('wizValRequireOther', function () {
-		return {
-			restrict: 'A',
-			require: 'ngModel',
-			scope: { elementsToCheck: '=wizValRequireOther' },
-			link: function (scope, elem, attrs, ngModel) {
+.directive('wizValRequireOther', function () {
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		scope: { elementsToCheck: '=wizValRequireOther', model: '=ngModel' },
+		link: function (scope, elem, attrs, ngModel) {
+	
+			//For DOM -> model validation
+			ngModel.$parsers.unshift(function (value) {
+				return validate(value);
+			});
 
-				//For DOM -> model validation
-				ngModel.$parsers.unshift(function (value) {
-					return validate(value);
-				});
-
-				//For model -> DOM validation
-				ngModel.$formatters.unshift(function (value) {
-					return validate(value);
-				});
-
-				function validate(value) {
-					if (typeof value === "undefined") value = "";
-					var valid = true;
-					if (typeof scope.elementsToCheck !== "undefined") {
-						for (var i = scope.elementsToCheck.length - 1; i >= 0; i--) {
-							if (!scope.elementsToCheck[i].$valid || value == "") {
-								valid = false;
-								break;
-							}
+			//For model -> DOM validation
+			ngModel.$formatters.unshift(function (value) {
+				return validate(value);
+			});
+			
+			if (typeof scope.elementsToCheck !== "undefined") {
+				scope.$watch('elementsToCheck',
+				function(n,o){
+				 console.log(n,o);
+				 validate(ngModel.$viewValue);
+				}, true);
+			}
+			
+			function validate(value) {
+				if (typeof value === "undefined") value = "";
+				var valid = true;
+				if (typeof scope.elementsToCheck !== "undefined") {
+					for (var i = 0; i <= scope.elementsToCheck.length - 1; i++) {
+						if (scope.elementsToCheck[i] == false || value == "") {
+							valid = false;
+							break;
 						}
 					}
 					ngModel.$setValidity('wizValRequireOther', valid);
