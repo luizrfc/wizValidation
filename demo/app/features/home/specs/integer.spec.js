26cd79c573605d361fbf7d3cedc0492a3802fb7d
Greg <gregory.pratt@me.com>
2014-04-22 16:16:55 +0100

    Added tests for file type validator #39

--- demo/app/features/home/specs/integer.spec.js
+++ demo/app/features/home/specs/integer.spec.js
@@ -9,10 +9,12 @@ describe('Integer validation', function () {
 	it('should allow empty field', function () {
 		expect(elem.getAttribute('class')).not.toMatch('ng-invalid');
 	});
+
 	it('should allow whole numbers', function () {
 		elem.sendKeys('1234567890');
 		expect(elem.getAttribute('class')).not.toMatch('ng-invalid');
 	});
+
 	it('should set invalid when not whole numbers', function () {
 		elem.sendKeys('0.5');
 		expect(elem.getAttribute('class')).toMatch('ng-invalid');
