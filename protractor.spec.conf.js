a72bd03e5610de44e31af8a72bf1f296834fecbd
Greg <gregory.pratt@me.com>
2014-04-22 16:19:24 +0100

    Update protractor.spec.conf.js

--- protractor.spec.conf.js
+++ protractor.spec.conf.js
@@ -9,7 +9,7 @@ exports.config = {
 
 	// Spec patterns are relative to the current working directly when
 	// protractor is called.
-	specs: ['demo/app/features/home/specs/file.spec.js'],
+	specs: ['demo/app/features/home/specs/*.spec.js'],
 
 	// Options to be passed to Jasmine-node.
 	jasmineNodeOpts: {
