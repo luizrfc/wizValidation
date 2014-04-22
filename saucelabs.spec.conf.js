97eeac9fab2f15495ff2826cf49237851d48ea0e
Greg <gregory.pratt@me.com>
2014-04-22 09:21:35 +0100

    Update saucelabs.spec.conf.js

--- saucelabs.spec.conf.js
+++ saucelabs.spec.conf.js
@@ -17,7 +17,7 @@ exports.config = {
 	// Options to be passed to Jasmine-node.
 	jasmineNodeOpts: {
 		showColors: true,
-		defaultTimeoutInterval: 60000
+		defaultTimeoutInterval: 120000
 	},
 
 	baseUrl: 'http://localhost:9001/'
