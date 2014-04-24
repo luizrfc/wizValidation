f1eacb609c500012094613ed052448b56e94ecfb
Greg <gregory.pratt@me.com>
2014-04-24 12:23:18 +0100

    Improved date of birth tests

--- demo/app/features/home/specs/dateOfBirth.spec.js
+++ demo/app/features/home/specs/dateOfBirth.spec.js
@@ -1,11 +1,9 @@
 describe('Date of birth validation', function () {
 
-	var today, testDate, elem;
+	var elem;
 
 	beforeEach(function () {
 		browser.get('demo/index.html');
-		today = new Date();
-		testDate = new Date();
 		elem = element(by.model('demo.dateOfBirth'));
 	});
 
@@ -14,18 +12,24 @@ describe('Date of birth validation', function () {
 	});
 
 	it('should allow birth dates of people 18', function () {
+		var today = new Date();
+		var testDate = new Date();
 		testDate.setFullYear(today.getFullYear() - 18);
 		elem.sendKeys('01/01/' + testDate.getFullYear());
 		expect(elem.getAttribute('class')).not.toMatch('ng-invalid');
 	});
 
 	it('should allow birth dates of people over 18', function () {
+		var today = new Date();
+		var testDate = new Date();
 		testDate.setFullYear(today.getFullYear() - 19);
 		elem.sendKeys('01/01/' + testDate.getFullYear());
 		expect(elem.getAttribute('class')).not.toMatch('ng-invalid');
 	});
 
 	it('should set invalid when younger than 18', function () {
+		var today = new Date();
+		var testDate = new Date();
 		testDate.setFullYear(today.getFullYear() - 17);
 		elem.sendKeys('01/01/' + testDate.getFullYear());
 		expect(elem.getAttribute('class')).toMatch('ng-invalid');
