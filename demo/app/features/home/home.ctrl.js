482e0a72853aa5fa7f4d4fee3076c20c744ae625
Greg <gregory.pratt@me.com>
2014-04-25 09:29:32 +0100

    Conditions tests updated

--- demo/app/features/home/home.ctrl.js
+++ demo/app/features/home/home.ctrl.js
@@ -3,9 +3,10 @@ angular.module('demo.features.home')
 	.controller('HomeCtrl', ['$scope', function ($scope) {
 
 		$scope.demo = {};		
+		$scope.testDate = new Date();
 		$scope.today = new Date();
 		$scope.addDay = function () {
-			$scope.today.setDate($scope.today.getDate() + 1);
+			$scope.testDate.setDate($scope.testDate.getDate() + 1);
 		};
 
 	}]);
